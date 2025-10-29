55e0a2a9ef54ee0a800cdcd0ee120a0884c77686
M Salman <salmanjatt1012@gmail.com>
2025-10-29 17:03:05 +0500

    api authentication

--- app/Http/kernel.php
+++ app/Http/kernel.php
@@ -34,8 +34,11 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            'throttle:api',
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
+
     ];
 
     /**
